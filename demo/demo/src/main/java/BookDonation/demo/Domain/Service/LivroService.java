373b95702a1e6235148c325d394decc20ee157a8
Fernando Gabriel <bielblacksouza@gmail.com>
2026-04-10 14:33:52 -0300

    Funções Editar e Excluir
    agora o CRUD está 100% funcional!

--- demo/demo/src/main/java/BookDonation/demo/Domain/Service/LivroService.java
+++ demo/demo/src/main/java/BookDonation/demo/Domain/Service/LivroService.java
@@ -4,6 +4,8 @@ import BookDonation.demo.Domain.Model.Livro;
 import BookDonation.demo.Domain.Model.ValueObjects.*;
 import BookDonation.demo.Domain.Repository.LivroRepository;
 import BookDonation.demo.presentation.DTO.LivroRequestDTO;
+import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +30,33 @@ public class LivroService {
         return livroRepository.save(novoLivro);
     }
 
+    @Transactional
+    public Livro atualizarLivro(Long id, LivroRequestDTO dto) {
+        Livro livroExistente = buscarPorId(id);
+
+        AnoLivro ano        = new AnoLivro(dto.anoLancamento());
+        Autor autor         = new Autor(dto.nomeAutor());
+        Descricao descricao = new Descricao(dto.textoDescricao());
+        Genero genero       = new Genero(dto.nomeGenero());
+        Idioma idioma       = new Idioma(dto.nomeIdioma());
+        Pagina pagina       = new Pagina(dto.quantidadePaginas());
+        StatusLivro status  = new StatusLivro(dto.statusInicial());
+
+        livroExistente.atualizarDados(dto.titulo(), ano, autor, descricao, genero, idioma, pagina, status);
+        return livroRepository.save(livroExistente);
+    }
+
+    public void excluirLivro(Long id) {
+        Livro livro = buscarPorId(id);
+        
+        livroRepository.delete(livro);
+    }
+
     public List<Livro> listarTodosOsLivros() {
         return livroRepository.findAll(); 
+    }
+
+    public Livro buscarPorId(Long id) {
+        return livroRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Livro não encontrado no banco de dados."));
     }
 }
--- demo/demo/src/main/java/BookDonation/demo/presentation/Controller/LivroController.java
+++ demo/demo/src/main/java/BookDonation/demo/presentation/Controller/LivroController.java
@@ -38,6 +38,31 @@ public class LivroController {
         }
     }
 
+    @GetMapping("/editar/{id}")
+    public String mostrarTelaEditar(@PathVariable Long id, Model model, RedirectAttributes attributes) {
+        try {
+            Livro livro = livroService.buscarPorId(id);
+            model.addAttribute("livro", livro);
+            return "PainelEditar"; 
+            
+        } catch (IllegalArgumentException e) {
+            attributes.addFlashAttribute("erro", "Não foi possível encontrar um livro com o ID #" + id);
+            return "redirect:/livros/painel"; // Devolve para o painel em segurança
+        }
+    }
+
+    @PostMapping("/editar/{id}")
+    public String atualizarLivro(@PathVariable Long id, LivroRequestDTO dto, RedirectAttributes attributes) {
+        try {
+            livroService.atualizarLivro(id, dto);
+            
+            attributes.addFlashAttribute("mensagem", "Livro atualizado com sucesso!");
+        } catch (Exception e) {
+            attributes.addFlashAttribute("erro", "Erro ao atualizar: " + e.getMessage());
+        }
+        return "redirect:/livros/painel";
+    }
+
     @GetMapping("/painel")
     public String mostrarPainelAdm(Model model) {
         List<Livro> listaDeLivros = livroService.listarTodosOsLivros();
@@ -45,5 +70,21 @@ public class LivroController {
         model.addAttribute("livros", listaDeLivros);
         
         return "PainelADM";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirLivro(@PathVariable Long id, RedirectAttributes attributes) {
+        try {
+            livroService.excluirLivro(id);
+            
+            attributes.addFlashAttribute("mensagem", "Livro excluído com sucesso!");
+            
+        } catch (IllegalArgumentException e) {
+            attributes.addFlashAttribute("erro", "Erro ao excluir: Não foi possível encontrar o livro #" + id);
+        } catch (Exception e) {
+            attributes.addFlashAttribute("erro", "Erro interno ao tentar excluir o livro.");
+        }
+        
+        return "redirect:/livros/painel"; 
     }
 }
